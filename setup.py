eb5dc7346268aa589ba1d723e627a7300eef8b9e
Mingde Yin <mingdeyin@gmail.com>
2023-02-17 00:04:29 -0500

    attempt to fix build script

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
-from pathlib import Path
+from os import path
 
 from setuptools import setup
 
 if __name__ == "__main__":
     # Make a relative path for the cffi module
-    cffi_dir = Path(__file__).parent / "src" / \
-        "trajectorize" / "c_ext_utils"
-    setup(cffi_modules=[str(cffi_dir / "build_c_extension.py:ffi")])
+    ext_dir = path.join("src", "trajectorize", "c_ext_utils",
+                        "build_c_extension.py:ffi")
+    setup(cffi_modules=[ext_dir])
