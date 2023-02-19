78e2735c918fbaa394e34ce3171ef3adbeed1251
Mingde Yin <mingdeyin@gmail.com>
2023-02-18 23:33:55 -0500

    update browser MVP to latest API

--- src/browser_interface/browser_main.c
+++ src/browser_interface/browser_main.c
@@ -10,12 +10,13 @@ int main(int argc, char *argv[])
     Body duna = kerbol_system_bodies[DUNA];
     double t1 = 5091552;
     double t2 = 10679760;
-    KeplerianElements transfer_orbit = planetary_transfer_orbit(
+    TransferOrbit transfer_orbit = get_transfer_orbit(
         kerbin, duna, t1, t2);
     Vector3 excess_velocity_at_kerbin = excess_velocity_at_body(
-        kerbin, transfer_orbit, t1);
+        transfer_orbit, DEPARTURE);
     double r_pe = 70000 + kerbin.radius;
-    double delta_v = delta_v_req(kerbin, vec_norm(excess_velocity_at_kerbin), r_pe);
+    double delta_v = ejection_capture_dv(
+        kerbin, excess_velocity_at_kerbin, r_pe);
 
     printf("Delta-v to eject from Kerbin: %.2f m/s\n", delta_v);
     printf("Time of transfer: %.0f seconds\n", t2 - t1);
